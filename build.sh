cb8a54298499d508b724134f0e43c7b112c35dfc
wjmn <jamin.wu07@gmail.com>
2023-10-22 18:56:46 -0400

    Build updated for crackle3

--- build.sh
+++
@@ -1,3 +0,0 @@
-dotnet publish -r linux-x64 -c Release -o ../Releases/linux /p:SelfContained=true /p:PublishSingleFile=true /p:PublishReadyToRun=true
-dotnet publish -r osx-x64 -c Release -o ../Releases/osx /p:SelfContained=true /p:PublishSingleFile=true /p:PublishReadyToRun=true
-dotnet publish -r win-x64 -c Release -o ../Releases/win /p:SelfContained=true /p:PublishSingleFile=true /p:PublishReadyToRun=true
